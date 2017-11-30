872c0d247bff9016efe708e330648d85d3c4ddfe
FabriGinseng <antoniofabriziofiume@libero.it>
2017-11-30 15:03:05 +0100

    aggiunto in model la classe dati

---
+++ AppOrdineMediciCaserta/AppOrdineMediciCaserta/Models/DatiEvento.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppOrdineMediciCaserta.Models
+{
+    class DatiEvento
+    {
+        public string Titolo { get; set; }
+        public string Sottotitolo { get; set; }
+        public string Data { get; set; }
+        public string Luogo { get; set; }
+        public string Informazioni { get; set; }
+        public string Relatori { get; set; }
+        public string Descrizione { get; set; }
+        public string Immagine { get; set; }
+    }
+}
+/
