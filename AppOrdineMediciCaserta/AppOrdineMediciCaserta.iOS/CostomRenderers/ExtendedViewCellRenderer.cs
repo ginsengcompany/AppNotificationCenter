490b6a9ca6b779c5833f390ce9b8eb670d7206d9
AlessioRivoli <pleyboy87@hotmail.it>
2017-12-12 15:18:43 +0100

    Lavoro sul CustomRender

---
+++ AppOrdineMediciCaserta/AppOrdineMediciCaserta.iOS/CostomRenderers/ExtendedViewCellRenderer.cs
@@ -0,0 +1,26 @@
+﻿using UIKit;
+using AppOrdineMediciCaserta.CostomControls;
+using AppOrdineMediciCaserta.iOS.CostomRenderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ExtendedViewCell), typeof(ExtendedViewCellRenderer))]
+
+namespace AppOrdineMediciCaserta.iOS.CostomRenderers
+{
+    class ExtendedViewCellRenderer : ViewCellRenderer
+    {
+        public override UITableViewCell GetCell(Cell item, UITableViewCell reusableCell, UITableView tv)
+        {
+            var cell = base.GetCell(item, reusableCell, tv);
+            var view = item as ExtendedViewCell;
+            cell.SelectedBackgroundView = new UIView
+            {
+                BackgroundColor = view.SelectedBackgroundColor.ToUIColor(),
+            };
+
+            return cell;
+        }
+
+    }
+}
