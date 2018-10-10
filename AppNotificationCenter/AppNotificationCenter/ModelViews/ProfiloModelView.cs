e1ab35c1447c560e26aa72c0dffd640bdb137262
AlessioRivoli <pleyboy87@hotmail.it>
2018-10-10 12:11:38 +0200

    Aggiunto profilo utente modifica e annulla

--- AppNotificationCenter/AppNotificationCenter/ModelViews/ProfiloModelView.cs
+++ AppNotificationCenter/AppNotificationCenter/ModelViews/ProfiloModelView.cs
@@ -5,15 +5,37 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using AppNotificationCenter.Database.Data;
 using AppNotificationCenter.Database.Models;
 using AppNotificationCenter.Models;
+using Xamarin.Forms;
 
 namespace AppNotificationCenter.ModelViews
 {
     public class ProfiloModelView:INotifyPropertyChanged
     {
         private TbUtente utenteProfilo;
+        private bool isEnabled=false;
+        private bool isEnabledModifica = true;
+        public bool IsEnabledModifica
+        {
+            get { return isEnabledModifica; }
+            set
+            {
+                OnPropertychanged();
+                isEnabledModifica = value;
+            }
+        }
+        public bool IsEnabled
+        {
+            get { return isEnabled; }
+            set
+            {
+                OnPropertychanged();
+                isEnabled = value;
+            }
+        }
 
         public TbUtente UtenteProfilo
         {
@@ -27,9 +49,44 @@ namespace AppNotificationCenter.ModelViews
     
         public ProfiloModelView()
         {
-          UtenteProfilo = UtenzaData.getUser();
+            IsEnabled = false;
+            isEnabledModifica = true;
+            UtenteProfilo = UtenzaData.getUser();
         }
-
+        public ICommand ModificaInfo
+        {
+            get
+            {
+                return new Command(() =>
+                {
+                    IsEnabled = true;
+                    isEnabledModifica = false;
+                });
+            }
+        }
+        public ICommand btnAnnulla
+        {
+            get
+            {
+                return new Command(() =>
+                {
+                    IsEnabled = false;
+                    isEnabledModifica = true;
+                    UtenteProfilo = UtenzaData.getUser();
+                });
+            }
+        }
+        public ICommand btnConferma
+        {
+            get
+            {
+                return new Command(() =>
+                {
+                    IsEnabled = false;
+                    isEnabledModifica = true;
+                });
+            }
+        }
         public event PropertyChangedEventHandler PropertyChanged;
         private void OnPropertychanged([CallerMemberName] string name = "")
         {
